a857744ebeec692533c01e1bcc60edd5bf9d8b01
Karen Zapata <kzapata0919@gmail.com>
2016-08-19 17:03:10 -0700

    Add container components

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    <div>
+      <h3>
+        Yo, world!
+      </h3>
+    </div>
+  `
+})
+
+export class App {}
